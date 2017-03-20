ca4df318bf02390907f4306b22a39ad698cf5e07
koffeinfriedhof <koffeinfriedhof@gmail.com>
2017-03-20 07:12:24 +0100

    provide system-installation

--- install.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-echo running command: "cmake CMakeLists.txt"
-cmake CMakeLists.txt
-echo *** running command: "make"
-make
-echo *** running command: "sudo make install"
-sudo make install
